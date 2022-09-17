dcca99a1809183e5694241739b084b9cb7ffb1ba
Ugaitz Urien <ugaitz.urien@gmail.com>
2022-09-17 10:18:45 +0200

    Close popup with Escape key (#3)

--- assets/js/image-viewer.js
+++ assets/js/image-viewer.js
@@ -1,27 +1,27 @@
 function openPhoto () {
     const img = this.querySelector('img')
-    const bigImgSrc = img.dataset.bigSrc
-    console.log('camera', img.dataset.camera)
-    console.log('iso', img.dataset.iso)
-    console.log('focal', img.dataset.focal)
-    console.log('aperture', img.dataset.aperture)
-    console.log('shutterSpeed', img.dataset.shutterSpeed)
-    console.log('bigSrc', img.dataset.bigSrc)
-    console.log('bigSrc', img.dataset.bigSrc)
     bigImg.setAttribute('src', img.dataset.bigSrc)
     photoDetail.classList.remove('hidden')
-//    photoDetail.innerHTML = `<img src="${img.dataset.bigSrc}" />`
 }
 let photoDetail
 let photoContainer
 let bigImg
 export default function init () {
-    // photo-list
-    // photo-list > li
     photoDetail = document.getElementById('photo-detail')
     photoDetail.addEventListener('click', (e) => {
         console.log(e)
         photoDetail.classList.add('hidden')
+    })
+    document.addEventListener('keydown', (evt) => {
+        let isEscape;
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc");
+        } else {
+            isEscape = (evt.keyCode === 27);
+        }
+        if (isEscape) {
+            photoDetail.classList.add('hidden')
+        }
     })
     photoContainer = photoDetail.querySelector('photo-container')
     bigImg = photoDetail.querySelector('img')
